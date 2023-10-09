9ea4e5c8f32f2ce4979544c8f718403779d4ac5f
DanCpp12 <144418307+DanCpp12@users.noreply.github.com>
2023-10-09 12:45:25 +0200

    .

--- The Racing Drift/Assets/resorces/prefab/data/menuData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MenuData : MonoBehaviour
-{
-    public int TrackID = 0;
-    public int CarID = 0;
-}
