f9cd32fae8f9cbfeb36fb5ba0f625bb83fc91faf
DanCpp12 <144418307+DanCpp12@users.noreply.github.com>
2023-10-09 12:45:48 +0200

    .

---
+++ The Racing Drift/Assets/resorces/prefab/data/menuData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuData : MonoBehaviour
+{
+    public int TrackID = 0;
+    public int CarID = 0;
+}
---
+++ The Racing Drift/Assets/scripts/Checkpoint Manager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public GameObject hitbox;
+    public TrackManeger trackManeger;
+    //public GameObject car;
+    public bool finish;
+    public bool Checkpoint;
+    public int lap = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        finish = true;
+        Checkpoint = true;
+        trackManeger.finishline();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        finish = false;
+    }
+    public bool Finish(bool checkpoints)
+    {
+        if (checkpoints == true) 
+        {
+            Checkpoint = false;
+            return true;
+        }
+        return false;
+    }
+    /*public bool split()
+    {
+        return;
+    }*/ 
+}
